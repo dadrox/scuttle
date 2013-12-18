db5a02f7ca07ca5e811fc2627277b63d73b965be
dadrox <cbwood@gmail.com>
2013-12-18 08:50:18 -0700

    Added a CountDownLatch, which pretty much delegates to a Java CountDownLatch

---
+++ scuttle/src/test/scala/com/dadrox/scuttle/concurrent/CountDownLatch.scala
@@ -0,0 +1,13 @@
+package com.dadrox.scuttle.concurrent
+
+import com.dadrox.scuttle.time._
+import java.util.concurrent.{ TimeUnit => JTimeUnit }
+
+class CountDownLatch(initialCount: Int) {
+    private val latch = new java.util.concurrent.CountDownLatch(initialCount)
+
+    def await(): Unit = latch.await()
+    def await(duration: Duration): Boolean = latch.await(duration.inMilliseconds(), JTimeUnit.MILLISECONDS)
+    def countDown() = latch.countDown()
+    def count(): Long = latch.getCount()
+}
