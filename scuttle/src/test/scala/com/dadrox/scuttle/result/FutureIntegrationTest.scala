89c1193df34a13696c03478c63232f7c9630e32e
dadrox <cbwood@gmail.com>
2014-02-28 07:33:58 -0700

    Made the test more resilient

--- scuttle/src/test/scala/com/dadrox/scuttle/result/FutureIntegrationTest.scala
+++ scuttle/src/test/scala/com/dadrox/scuttle/result/FutureIntegrationTest.scala
@@ -10,11 +10,13 @@ class FutureIntegrationTest extends Fictus {
     @Test
     def join_success_waits_for_everything {
         val numberOfFutures = 4
+        val sleepDuration = 100 ms
+
         val start = Time.now
         val count = new AtomicInteger(0)
 
         def newFuture(i: Int) = Future {
-            Thread.sleep(100)
+            Thread.sleep(sleepDuration.inMs)
             count.incrementAndGet()
             Success(i)
         }
@@ -23,7 +25,7 @@ class FutureIntegrationTest extends Fictus {
         Future.join(fs).await mustEqual Success(Void)
 
         val time = Time.now.since(start)
-        val timeout = 200.ms
+        val timeout = sleepDuration * numberOfFutures
         if (time > timeout) fail(s"Futures must all finish within $timeout, but finished in $time")
 
         count.get mustEqual numberOfFutures
